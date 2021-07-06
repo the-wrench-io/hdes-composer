9fbe9abe310e903dc854e3de47062168d28b2c4b
Olev Mutso <olev.mutso@resys.io>
2021-07-06 16:01:37 +0300

    dev setting

---
+++ src/core/index.ts
@@ -0,0 +1,8 @@
+import Resource from '../resource-editor';
+import Hdes from '../hdes-service';
+import Vis from '../vis';
+import { theme } from '../themes';
+import messages from '../intl';
+
+
+export { Resource, Hdes, Vis, theme, messages };
