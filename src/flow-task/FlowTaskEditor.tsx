3f2cecc46b296ffe930b8d85d2b54495774d42ba
Olev Mutso <olev.mutso@resys.io>
2021-09-15 12:11:00 +0300

    task editor

--- src/flow-task/FlowTaskEditor.tsx
+++ src/flow-task/FlowTaskEditor.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
 import { createStyles, makeStyles } from '@material-ui/styles';
-import { Theme } from '@material-ui/core';
+import { Theme, Grid } from '@material-ui/core';
 
 import { CodeEditor, Hdes } from './deps';
 
 
-const useStyles = makeStyles((_theme: Theme) =>
+const useStyles = (size: { width: number, height: number }) => makeStyles((_theme: Theme) =>
   createStyles({
     root: {
-    }
+      flexGrow: 1,
+    },
+    editor: {
+      height: size.height,
+    },
   }),
-);
+)();
 
 interface FlowTaskEditorProps {
   models: Hdes.ModelAPI.Models;
@@ -40,10 +44,13 @@ const FlowTaskEditor: React.FC<FlowTaskEditorProps> = (props) => {
   }
 
   return (<div className={classes.root}>
-    <CodeEditor.Provider mode="ft" theme={props.theme.mode} onCommands={props.onChanges}>
-      {parser?.src}
-    </CodeEditor.Provider>
-
+    <Grid container spacing={0}>
+      <Grid item xs={6} className={classes.editor}>
+        <CodeEditor.Provider mode="ft" theme={props.theme.mode} onCommands={props.onChanges}>
+          {parser?.src}
+        </CodeEditor.Provider>
+      </Grid>
+    </Grid>
   </div>);
 }
 
