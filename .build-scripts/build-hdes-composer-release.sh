5bb71f4701a959e0e17eaa872d2f546aac1b4170
Olev Mutso <olev.mutso@resys.io>
2021-12-27 12:47:57 +0200

    yarn

--- .build-scripts/build-hdes-composer-release.sh
+++ .build-scripts/build-hdes-composer-release.sh
@@ -10,17 +10,13 @@ if [[ "${last_release_commit_hash}" = "${GITHUB_SHA}" ]]; then
      #exit 0
 fi
 
-
 # Config GIT
 echo "Setup git user name to '$GIT_USER' and email to '$GIT_EMAIL'"
 git config --global user.name "$GIT_USER";
 git config --global user.email "$GIT_EMAIL";
 
 # Checkout
-git reset --hard
-git fetch --all
 git branch -a --contains ${GITHUB_SHA} --format="%(refname)"
-
 
 readonly local refname=$(git branch -a --contains ${GITHUB_SHA} --format="%(refname)" | head -1)
 if [[ "${refname}" = "refs/heads/main" ]]; then
@@ -29,22 +25,26 @@ else
      readonly local branch=${refname#refs/remotes/origin/}
 fi
 
-# resolve versions
-git checkout ${branch}
-readonly local PROJECT_VERSION=$(node -e "console.log(require('./package.json').version);")
-yarn version --patch
-readonly local PROJECT_VERSION_NEXT=$(node -e "console.log(require('./package.json').version);")
 
-# Log
-echo "Git checkout refname: '${refname}' branch: '${branch}' commit: '${GITHUB_SHA}'"
-echo "Project version: '${PROJECT_VERSION}' next: '${PROJECT_VERSION_NEXT}'"
-
-# Tag and publish
+# yarn
+corepack enable
+yarn set version 3.1.1
+echo "Current yarn version: $(yarn -v)"
+yarn plugin import version
 yarn install
 yarn build
-yarn publish --new-version ${PROJECT_VERSION_NEXT}
 
+# Publish and Tag
+readonly local PROJECT_VERSION=$(node -e "console.log(require('./package.json').version);")
+echo "Git checkout refname: '${refname}' branch: '${branch}' commit: '${GITHUB_SHA}'"
+echo "Project version: '${PROJECT_VERSION}'"
+git tag -a ${PROJECT_VERSION} -m "Release: '${PROJECT_VERSION}'"
+yarn npm publish --access public
+git push origin --tags
+
+# Next
+yarn version patch
+readonly local PROJECT_VERSION_NEXT=$(node -e "console.log(require('./package.json').version);")
+git commit -am "After release: '${PROJECT_VERSION_NEXT}'"
 git push origin ${branch}
-
-git tag -a ${PROJECT_VERSION_NEXT} -m "release ${PROJECT_VERSION_NEXT}"
-git push origin ${PROJECT_VERSION_NEXT}
+echo "Released: '${PROJECT_VERSION}', now: '${PROJECT_VERSION_NEXT}'"
