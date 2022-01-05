316e23aff16bfe89ec3beb6f8837a7d07328c65b
Olev Mutso <olev.mutso@resys.io>
2022-01-05 16:49:27 +0200

    service edit

--- src/core/service/ServiceEdit.tsx
+++ src/core/service/ServiceEdit.tsx
@@ -6,13 +6,14 @@ import { Client, Composer } from '../context';
 
 
 const ServiceEdit: React.FC<{service: Client.Entity<Client.AstService>}> = ({service}) => {
-  const { actions } = Composer.useComposer();
+  const { actions, session } = Composer.useComposer();
 
   const handleChange = (value: string | undefined) => {
     actions.handlePageUpdate(service.id, [{ type: "SET_BODY", value: value }])
   }
   
-  const src = service.ast?.value;
+  const update = session.pages[service.id];
+  const src = update && update.value.length > 0 ? update.value[0].value : service.ast?.value;
 
   return (<Box height="calc(100vh - 64px)">
     <CodeEditor id={service.id} mode="groovy" src={src ? src : "#--failed-to-parse"} onChange={handleChange} />
