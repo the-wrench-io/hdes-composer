7fc6a8b0efb5346220d837c41a7b061a2394dc10
Olev Mutso <olev.mutso@resys.io>
2022-10-05 12:41:01 +0300

    rel

--- src/core/index.ts
+++ src/core/index.ts
@@ -6,10 +6,11 @@ import Toolbar from './Toolbar';
 import Client from './client';
 
 import { Composer } from './context'; 
+import version from './version';
 
 const ComposerProvider = Composer.Provider;
 const useComposer = Composer.useComposer;
 
 
-export { messages, Main, Secondary, Toolbar, ComposerProvider, useComposer, Composer };
+export { messages, Main, Secondary, Toolbar, ComposerProvider, useComposer, Composer, version };
 export default Client;
